739760f13a7769de0b95633d69cd28cd0a892c94
josefuccef <josefuccef@gmail.com>
2025-02-14 15:49:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// follow me on github
+// https://github.com/arbiprog
+// the website designed by :@abdulldsgnr
+function msg() {
+    document.getElementById("show").style.top = "0px";
+}
+function msg2() {
+    document.getElementById("show").style.top = "-610px";
+}
+
+function dwn() {
+    const target = document.querySelector("#btn");
+    target.scrollIntoView({ behavior: "smooth" });
+}
+
+document.querySelector("#xpola").onclick = () => {
+    window.location.href =
+        "https://www.mediafire.com/file/o2hs1oc5xyjuops/xPola_Player_v4.0.apk/file?dkey=6q34e3pmnpd&r=1459";
+};
+document.querySelector("#match").onclick = () => {
+    window.location.href =
+        "https://apk.e-droid.net/apk/app3461147-q7mr4p.apk?v=5";
+};
